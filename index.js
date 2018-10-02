876f68578d4b375205d40ed694322a07c393ebeb
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-10-02 14:32:20 -0700

    game logic complete

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 (function() {
     let shuffleURL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
     let deckId = '';
-    // let drawURL = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
     let controls = document.querySelector('#controls');
-    let dealerCard = document.querySelector('#dealer');
-    let playerCard = document.querySelector('#player');
+    let dealerCard = {
+        element: document.querySelector('#dealer'),
+        value: null
+    };
+    let playerCard = {
+        element: document.querySelector('#player'),
+        value: null
+    };
+
     // Retrieve playing Deck
     axios.get(shuffleURL)
         .then( (response) => {
@@ -18,11 +24,31 @@
         console.log('you clicked shuffle');
     };
 
-    function drawCard(element) {
-        axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+    function parseValue(string) {
+        switch (string) {
+            case 'KING':
+                return 13
+                break;
+            case 'QUEEN':
+                return 12
+                break;
+            case 'JACK':
+                return 11
+                break;
+            case 'ACE':
+                return 14
+                break;
+            default:
+                return Number(string)
+                break;
+        }
+    }
+
+    async function drawCard(player) {
+        await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
             .then( res => {
-                // console.log(res.data.cards[0].image);
-                element.src = res.data.cards[0].image;
+                player['element'].src = res.data.cards[0].image;
+                player['value'] = parseValue(res.data.cards[0].value);
             })
             .catch( err => {
                 console.log(err);
@@ -31,7 +57,19 @@
     // Game Logic
     function playGuess(event) {
         // console.log(`you clicked ${event.target.id}`);
-        drawCard(playerCard);
+        drawCard(playerCard)
+            .then( res => {
+                switch (event.target.id) {
+                    case 'high':
+                        playerCard.value > dealerCard.value ? console.log(true) : console.log(false);
+                        break;
+                    case 'low':
+                        playerCard.value < dealerCard.value ? console.log(true) : console.log(false);
+                        break;
+                    default:
+                        break;
+                }
+            });
     }
     // Event Handlers
     function onControlsClick(event) {
